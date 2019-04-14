d5d4e28faad3a61534db518dceeca8007a1b4fa9
root <root@LAPTOP-PMI62AAT.localdomain>
2019-04-14 13:22:51 +0200

    Boxplots and LS performancetests

--- src/main.py
+++ src/main.py
@@ -5,12 +5,13 @@ import argparse
 import sys
 import os
 import fnmatch
-import np
+import numpy as np
+import re
 import data
 import time
 import solution
 # import solverCH
-# import solverLS
+from solverLS import SolverLS
 import solverCHH
 import solverNN
 import utilities
@@ -23,7 +24,9 @@ from two_opt import TwoOPT
 from cluster_localsearch import ClusterOPT
 from local_search import LocalSearch
 
+import matplotlib
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # Used for table creation
 instance_times_costs = []
@@ -100,17 +103,29 @@ def solve(instance, alg, config):
     return sol
 
 
+def boxplotter(filename):
+    dataframe = pd.read_csv(filename+'.dat', ' ', na_values='.')
+    # color = {'boxes': 'DarkGreen', 'whiskers': 'DarkOrange', 'medians': 'DarkBlue', 'caps': 'Gray'}
+    slicer = dataframe.iloc[:,[0,1,2]]
+    bp = slicer.boxplot(column='Cost', by='Instance')
+    axes = plt.gca()
+    # axes.set_ylim([0,25000])
+    plt.autoscale(enable=True, axis='y')
+    plt.title(filename)
+    plt.savefig(filename+'_boxplot.png')
+
 # Print the time it takes to resolve and the cost of the algorithm into a .dat file
 # This will make it possible for LaTeX to make the plots
 # Draw graphs to show the performance
 def performance_testing():
-    
     # Heuristics
     h_alg = [solverNN.algorithm, solverCHH.algorithm]
     h_alg_name = ["solverNN.algorithm", "solverCHH.algorithm"]
+    ls_alg_name = ["SolverLS_close_index_route_swap", "SolverLS_all_combination_route_swap",
+                            "TwoOPT"]
     try:
         for (idx, algo) in enumerate(h_alg):
-            with open(h_alg_name[idx]+'.dat', "a") as filehandle:
+            with open('../results/'+h_alg_name[idx]+'.dat', "a") as filehandle:
                 filehandle.write("{} {} {}\n".format("Instance", "Cost", "Time"))
                 for path, subdirs, files in os.walk('../data'):
                     for name in files:
@@ -119,91 +134,93 @@ def performance_testing():
                             instance = data.Data(os.path.join(path, name))
                             ch = ConstructionHeuristics(instance, algo)
                             sol = ch.construct(60-t0)
-
-                            filehandle.write("{} {} {}".format(name, sol.cost(), round(t0,2)))
+                            splitted_name = re.compile("^[a-zA-Z]+").findall(name)
+                            filehandle.write("{} {} {}".format(splitted_name[0], sol.cost(), round(t0,2)))
                             filehandle.write("\n")
+                            ls_alg = [SolverLS(sol).run_first, SolverLS(sol).run_second,
+                                            TwoOPT(sol).run]
+                                            
+                            
+                            for (ls_idx, l_alg) in enumerate(ls_alg):
+                                with open('../results/'+ls_alg_name[ls_idx]+'_'+h_alg_name[idx]+'.dat', "a") as ls_fh:
+                                    if os.stat('../results/'+ls_alg_name[ls_idx]+'_'+h_alg_name[idx]+'.dat').st_size == 0:
+                                        ls_fh.write("{} {} {}\n".format("Instance", "Cost", "Time"))
+                                    t0 = time.clock()
+                                    ls = LocalSearch(solution=sol, alg=l_alg)
+                                    try:
+                                        sol = ls.construct(60-t0)
+                                    except TimeOutExeption as e:
+                                        print("timeout")
+                                        sol = e.solution
+                                    ls_fh.write("{} {} {}".format(splitted_name[0], sol.cost(), round(t0,2)))
+                                    ls_fh.write("\n")
+                                    ls_fh.close()
                 filehandle.close()
+                boxplotter('../results/'+h_alg_name[idx])
+                for ls_idx in range(len(ls_alg_name)):
+                    boxplotter('../results/'+ls_alg_name[ls_idx]+'_'+h_alg_name[idx])
 
     except TimeOutExeption as e:
         print("timeout")
         sol = e.solution
 
-def boxplotter():
-    # Fixing random state for reproducibility
-    np.random.seed(19680801)
-
-    # fake up some data
-    spread = np.random.rand(50) * 100
-    center = np.ones(25) * 50
-    flier_high = np.random.rand(10) * 100 + 100
-    flier_low = np.random.rand(10) * -100
-    data = np.concatenate((spread, center, flier_high, flier_low))
-
-    fig, axs = plt.subplots(2, 3)
-
-    # basic plot
-    axs[0, 0].boxplot(data)
-    axs[0, 0].set_title('basic plot')
-
 def main(argv):
-    boxplotter()
-# def main(argv):
 
-#     parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-#     parser.add_argument('-o', action='store',
-#                         dest='output_file',
-#                         help='The file where to save the solution and, in case, plots')
+    parser.add_argument('-o', action='store',
+                        dest='output_file',
+                        help='The file where to save the solution and, in case, plots')
 
-#     parser.add_argument('-t', action='store',
-#                         dest='time_limit',
-#                         type=int,
-#                         required=True,
-#                         help='The time limit')
+    parser.add_argument('-t', action='store',
+                        dest='time_limit',
+                        type=int,
+                        required=True,
+                        help='The time limit')
 
-#     parser.add_argument('-s', dest="split_route", action='store_true',
-#                         help='Split the route to different subplot')
+    parser.add_argument('-s', dest="split_route", action='store_true',
+                        help='Split the route to different subplot')
 
-#     parser.add_argument('-g', dest="graphic_sol", action='store_true',
-#                         help='graphical solution')
+    parser.add_argument('-g', dest="graphic_sol", action='store_true',
+                        help='graphical solution')
 
-#     parser.add_argument('-instance_file', action='store',
-#                         help='The path to the file of the instance to solve')
+    parser.add_argument('-instance_file', action='store',
+                        help='The path to the file of the instance to solve')
 
-#     parser.add_argument('-all', action='store',
-#                         dest='all',
-#                         help='The file where to save the solution and, in case, plots')
+    parser.add_argument('-all', action='store',
+                        dest='all',
+                        help='The file where to save the solution and, in case, plots')
 
-#     parser.add_argument('-performancetest', action='store',
-#                         dest='performancetest',
-#                         help='The file where to save the solution and, in case, plots')
+    parser.add_argument('-performancetest', action='store',
+                        dest='performancetest',
+                        help='The file where to save the solution and, in case, plots')
 
-#     config = parser.parse_args()
+    config = parser.parse_args()
 
-#     print('instance_file    = {!r}'.format(config.instance_file))
-#     print('output_file      = {!r}'.format(config.output_file))
-#     print('time_limit       = {!r}'.format(config.time_limit))
-#     print('graphical solution= {!r}'.format(config.graphic_sol))
-#     print('split route       = {!r}'.format(config.split_route))
+    print('instance_file    = {!r}'.format(config.instance_file))
+    print('output_file      = {!r}'.format(config.output_file))
+    print('time_limit       = {!r}'.format(config.time_limit))
+    print('graphical solution= {!r}'.format(config.graphic_sol))
+    print('split route       = {!r}'.format(config.split_route))
 
-#     if config.performancetest:
-#         performance_testing()
-#     else:
-#         instance = data.Data(config.instance_file)
-#         # alg = solverNN.algorithm
-#         alg = solverCHH.algorithm
-#         sol = solve(instance, alg, config)
-#         if config.output_file is not None:
-#             if config.graphic_sol:
-#                 plt.figure(figsize=(20, 10))
-#                 plt.rcParams.update({'font.size': 22})
-#                 sol.plot_routes(split=config.split_route,
-#                                 output_filename=config.output_file+'_sol'+'.png')
-#             sol.write_to_file(config.output_file+'.sol')
-#             # print(instance_times_costs)
-#             # sol.plot_table(config.output_file+'_tbl', instance.instance_name, instance_times_costs)
-#         print("{} routes with total cost {:.1f}"
-#                 .format(len(sol.routes), sol.cost()))
+    if config.performancetest:
+        performance_testing()
+    else:
+        instance = data.Data(config.instance_file)
+        # alg = solverNN.algorithm
+        alg = solverCHH.algorithm
+        sol = solve(instance, alg, config)
+        if config.output_file is not None:
+            if config.graphic_sol:
+                plt.figure(figsize=(20, 10))
+                plt.rcParams.update({'font.size': 22})
+                sol.plot_routes(split=config.split_route,
+                                output_filename=config.output_file+'_sol'+'.png')
+            sol.write_to_file(config.output_file+'.sol')
+            # print(instance_times_costs)
+            # sol.plot_table(config.output_file+'_tbl', instance.instance_name, instance_times_costs)
+        print("{} routes with total cost {:.1f}"
+                .format(len(sol.routes), sol.cost()))
 
 
 if __name__ == "__main__":
--- src/solverLS.py
+++ src/solverLS.py
@@ -5,103 +5,111 @@ import data
 import solution
 import sys
 
-class LocalSearch:
-    total_distance = 0
-    def __init__(self,instance):
-        self.instance = instance
+class SolverLS:
+    # total_distance = 0
+    solution = None
+    calculated_route_capacity = []
+    def __init__(self,solution):
+        self.solution = solution
 
     def euclideanDistance(self, coordinate1, coordinate2):
         return pow(pow(coordinate1.x - coordinate2.x, 2) + pow(coordinate1.y - coordinate2.y, 2), .5)
 
+    # Kind of "dirty" code to get the job done
+    def calculate_routes_capacities(self):
+        result = 0
+        del self.calculated_route_capacity[:]
+        print(len(self.solution.routes))
+        for i in range(len(self.solution.routes)):
+            for j in range(len(self.solution.routes[i])):
+                result += self.solution.routes[i][j]
+            self.calculated_route_capacity += [result]
+            result = 0
+
     # First attempt - Compare two routes
     # Husk at beregne til det næste punkt, som der bliver byttet ud med
-    def local_search2(self, start_sol, current_total_distance, time_left):
-        "start_sol = 2D-array solution/routes"
-        t0 = time.clock()
-        #print("BEFORE: ", start_sol.routes)
+    def close_index_route_swap(self):
+        "self.solution = 2D-array solution/routes"
+        # t0 = time.clock()
         counter = 0
         exit_criteria = True
-        while exit_criteria:
-            for i in range(len(start_sol.routes)-1):
-                for j in range(len(start_sol.routes[i])):
-                    try:
-                        # Triangle Inequality                    
-                        sidea_dist_r1 = self.euclideanDistance(self.instance.nodes[start_sol.routes[i][j]]["pt"], self.instance.nodes[start_sol.routes[i][j+1]]["pt"])
-                        sideb_dist_r1 = self.euclideanDistance(self.instance.nodes[start_sol.routes[i][j+1]]["pt"], self.instance.nodes[start_sol.routes[i+1][j+1]]["pt"])
-                        sidec_dist_r1 = self.euclideanDistance(self.instance.nodes[start_sol.routes[i][j]]["pt"], self.instance.nodes[start_sol.routes[i+1][j+1]]["pt"])
-                        sidea_dist_r2 = self.euclideanDistance(self.instance.nodes[start_sol.routes[i+1][j]]["pt"], self.instance.nodes[start_sol.routes[i+1][j+1]]["pt"])
-                        sidec_dist_r2 = self.euclideanDistance(self.instance.nodes[start_sol.routes[i+1][j]]["pt"], self.instance.nodes[start_sol.routes[i][j+1]]["pt"])
-                        if sidea_dist_r1 + sideb_dist_r1 > sidec_dist_r1: #Triangle Inequality by contradiction withhold
-                            if sidec_dist_r1 < sidea_dist_r1: # We can make a swap                            
-                                if (start_sol.route_rq_slack[i] - self.instance.nodes[start_sol.routes[i][j+1]]["rq"] + self.instance.nodes[start_sol.routes[i+1][j+1]]["rq"]) <= self.instance.capacity:
-                                    tempPointer = start_sol.routes[i+1][j+1]
-                                    start_sol.routes[i+1][j+1] = start_sol.routes[i][j+1]
-                                    start_sol.routes[i][j+1] = tempPointer
-                                    current_total_distance = current_total_distance - sidea_dist_r1 + sidec_dist_r1 - sidea_dist_r2 + sidec_dist_r2
-                                else:
-                                    counter += 1
-                                    if counter == 5:
-                                        exit_criteria = False
-                                    pass
-                                    #print("Not enough capacity")
-                    except IndexError:
-                        pass
-                            #print('Out of Range')
+        self.calculate_routes_capacities()
+        # print("SOLUTION: ", self.calculated_route_capacity)
+        for i in range(len(self.solution.routes)-1):
+            for j in range(len(self.solution.routes[i])):
+                try:
+                    # Triangle Inequality                    
+                    sidea_dist_r1 = self.euclideanDistance(self.solution.instance.nodes[self.solution.routes[i][j]]["pt"], self.solution.instance.nodes[self.solution.routes[i][j+1]]["pt"])
+                    sideb_dist_r1 = self.euclideanDistance(self.solution.instance.nodes[self.solution.routes[i][j+1]]["pt"], self.solution.instance.nodes[self.solution.routes[i+1][j+1]]["pt"])
+                    sidec_dist_r1 = self.euclideanDistance(self.solution.instance.nodes[self.solution.routes[i][j]]["pt"], self.solution.instance.nodes[self.solution.routes[i+1][j+1]]["pt"])
+                    # sidea_dist_r2 = self.euclideanDistance(self.solution.instance.nodes[self.solution.routes[i+1][j]]["pt"], self.solution.instance.nodes[self.solution.routes[i+1][j+1]]["pt"])
+                    # sidec_dist_r2 = self.euclideanDistance(self.solution.instance.nodes[self.solution.routes[i+1][j]]["pt"], self.solution.instance.nodes[self.solution.routes[i][j+1]]["pt"])
+                    if sidea_dist_r1 + sideb_dist_r1 > sidec_dist_r1: #Triangle Inequality by contradiction withhold
+                        if sidec_dist_r1 < sidea_dist_r1: # We can make a swap                            
+                            if (self.calculated_route_capacity[i] - self.solution.instance.nodes[self.solution.routes[i][j+1]]["rq"] + self.solution.instance.nodes[self.solution.routes[i+1][j+1]]["rq"]) <= self.solution.instance.capacity:
+                                tempPointer = self.solution.routes[i+1][j+1]
+                                self.solution.routes[i+1][j+1] = self.solution.routes[i][j+1]
+                                self.solution.routes[i][j+1] = tempPointer
+                                # current_total_distance = current_total_distance - sidea_dist_r1 + sidec_dist_r1 - sidea_dist_r2 + sidec_dist_r2
+                            else:
+                                pass     
+                except IndexError:
+                    pass
                     
-                    if time.clock() - t0 > time_left:
-                        sys.stdout.write("Time expired")
-                        return start_sol
+                    
+                    # if time.clock() - t0 > time_left:
+                    #     sys.stdout.write("Time expired")
+                    #     return self.solution
 
-        #print("AFTER: ", start_sol.routes)
-        self.total_distance = current_total_distance
-        return start_sol
+        # self.total_distance = current_total_distance
+        # return self.solution
 
 
-    #Første loop:
-    #   Index for at holde styr på den enkelte rute, som man er igang med
-
-    #Andet loop:
-    #   Index til hvert element i første rute
-
-    #Tredje loop:
-	#   Index til hvert element i anden rute
-    # Husk at beregne til det næste punkt, som der bliver byttet ud med
-    def local_search3(self, start_sol, current_total_distance, time_left):
-        "start_sol = 2D-array solution/routes"
-        t0 = time.clock()
-        #print("BEFORE: ", start_sol.routes)
-        exit_criteria = True
-        #while exit_criteria:
-        for i in range(len(start_sol.routes)-1):
-            if i+1 < len(start_sol.routes):
-                for j in range(len(start_sol.routes[i])-1):
-                    for k in range(len(start_sol.routes[i+1])-1):
+    # First loop:
+    #   Index to keep track of the single route you are processing
+    # Second loop:
+    #   Index for each element in first route being compared
+    # Third loop:
+    #   Index for each element in the second route being compared
+    def all_combination_route_swap(self):
+        "self.solution = 2D-array solution/routes"
+        # t0 = time.clock()
+        for i in range(len(self.solution.routes)-1):
+            if i+1 < len(self.solution.routes):
+                for j in range(len(self.solution.routes[i])-1):
+                    for k in range(len(self.solution.routes[i+1])-1):
                         try:
-                            sidea_r1 = self.euclideanDistance(self.instance.nodes[start_sol.routes[i][k]]["pt"], self.instance.nodes[start_sol.routes[i][k+1]]["pt"])
-                            #sideb_r1 = self.euclideanDistance(self.instance.nodes[start_sol.routes[i][k+1]]["pt"], self.instance.nodes[start_sol.routes[i+1][k+1]]["pt"])
-                            sidec_r1 = self.euclideanDistance(self.instance.nodes[start_sol.routes[i][k]]["pt"], self.instance.nodes[start_sol.routes[i+1][k+1]]["pt"])
+                            sidea_r1 = self.euclideanDistance(self.solution.instance.nodes[self.solution.routes[i][k]]["pt"], self.solution.instance.nodes[self.solution.routes[i][k+1]]["pt"])
+                            sidec_r1 = self.euclideanDistance(self.solution.instance.nodes[self.solution.routes[i][k]]["pt"], self.solution.instance.nodes[self.solution.routes[i+1][k+1]]["pt"])
                             
                             # Route two to equalize the swap in route one 
-                            sidea_r2 = self.euclideanDistance(self.instance.nodes[start_sol.routes[i+1][k]]["pt"], self.instance.nodes[start_sol.routes[i+1][k+1]]["pt"])
+                            sidea_r2 = self.euclideanDistance(self.solution.instance.nodes[self.solution.routes[i+1][k]]["pt"], self.solution.instance.nodes[self.solution.routes[i+1][k+1]]["pt"])
                             # Side B Route 2 is the same as Side B Route 1
-                            sidec_r2 = self.euclideanDistance(self.instance.nodes[start_sol.routes[i+1][k]]["pt"], self.instance.nodes[start_sol.routes[i+1][k+1]]["pt"])
+                            sidec_r2 = self.euclideanDistance(self.solution.instance.nodes[self.solution.routes[i+1][k]]["pt"], self.solution.instance.nodes[self.solution.routes[i+1][k+1]]["pt"])
 
                             if sidea_r1 > sidec_r1:
                                 # Keeping the MAX capacity
-                                if (start_sol.route_rq_slack[i] - self.instance.nodes[start_sol.routes[i][k+1]]["rq"] + self.instance.nodes[start_sol.routes[i+1][k+1]]["rq"]) <= self.instance.capacity:
-                                    if (start_sol.route_rq_slack[i+1] - self.instance.nodes[start_sol.routes[i+1][k+1]]["rq"] + self.instance.nodes[start_sol.routes[i][k+1]]["rq"]) <= self.instance.capacity:
-                                        if start_sol.routes[i+1][k+1] != 0 and start_sol.routes[i][k+1] != 0:
-                                            tempPointer = start_sol.routes[i+1][k+1]
-                                            start_sol.routes[i+1][k+1] = start_sol.routes[i][k+1]
-                                            start_sol.routes[i][k+1] = tempPointer
-                                            current_total_distance = current_total_distance - sidea_r1 + sidec_r1 - sidea_r2 + sidec_r2
+                                if (self.calculated_route_capacity[i] - self.solution.instance.nodes[self.solution.routes[i][k+1]]["rq"] + self.solution.instance.nodes[self.solution.routes[i+1][k+1]]["rq"]) <= self.solution.instance.capacity:
+                                    if (self.calculated_route_capacity[i] - self.solution.instance.nodes[self.solution.routes[i+1][k+1]]["rq"] + self.solution.instance.nodes[self.solution.routes[i][k+1]]["rq"]) <= self.solution.instance.capacity:
+                                        if self.solution.routes[i+1][k+1] != 0 and self.solution.routes[i][k+1] != 0:
+                                            tempPointer = self.solution.routes[i+1][k+1]
+                                            self.solution.routes[i+1][k+1] = self.solution.routes[i][k+1]
+                                            self.solution.routes[i][k+1] = tempPointer
+                                            # current_total_distance = current_total_distance - sidea_r1 + sidec_r1 - sidea_r2 + sidec_r2
                         except IndexError:
                             continue
                     
-            if time.clock() - t0 > time_left:
-                sys.stdout.write("Time expired")
-                return start_sol
+            # if time.clock() - t0 > time_left:
+            #     sys.stdout.write("Time expired")
+            #     return self.solution
 
-        #print("AFTER: ", start_sol.routes)
-        self.total_distance = current_total_distance
-        return start_sol
+        # self.total_distance = current_total_distance
+        # return self.solution
+
+    def run_first(self):
+        self.close_index_route_swap()
+        return self.solution
+
+    def run_second(self):
+        self.all_combination_route_swap()
+        return self.solution
