8aa7cf639195b7daed46dee252b026b5180e669a
Narongrit Unwerawattana <narongrit.kie@gmail.com>
2019-04-25 09:03:14 +0200

    fix timeout handling

--- src/solver.py
+++ src/solver.py
@@ -1,5 +1,4 @@
-import time
-import itertools
+import math
 
 from data import Data
 import solution
@@ -26,7 +25,34 @@ class ConstructionHeuristics:
 
     def construct(self, time_left):
         signal.signal(signal.SIGALRM, self.timeoutHandler)
-        signal.alarm(int(time_left))
+        signal.alarm(math.ceil(time_left))
+        return self.canonical_solution()
+
+    def canonical_solution(self):
+        self.solution = solution.Solution(self.instance)
+        self.solution = self.alg(self.instance, self.solution)
+        return self.solution
+
+
+class MetaHeuristic:
+
+    instance = None
+    alg = None
+    solution = None
+
+    def __init__(self, instance=None, alg=None):
+        assert instance is not None
+        assert alg is not None
+        self.instance = instance
+        self.alg = alg
+        self.solution = solution.Solution(self.instance)
+
+    def timeoutHandler(self, signum, frame):
+        raise TimeOutExeption(self.solution)
+
+    def construct(self, time_left):
+        signal.signal(signal.SIGALRM, self.timeoutHandler)
+        signal.alarm(math.ceil(time_left))
         return self.canonical_solution()
 
     def canonical_solution(self):
